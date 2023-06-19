859f64197f512c2b096a620d30d237598f56abc8
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-06-19 22:05:14 +0400

    add about me component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,54 @@
-import SocialMedia from "./SocialMedia"
-
+import SocialMedia from "./SocialMedia";
+import styled from "styled-components";
+import myPhoto from "../assets/images/main-character.jpg";
 
 export default function Header() {
-    return(
-        <>
-        <SocialMedia />
-        </>
-    )
+  return (
+    <Container>
+      <SocialMedia />
+      <div className="imgBox">
+        <img src={myPhoto} alt="" />
+      </div>
+      <p className="name">
+        Nice to meet you! I'm <span>Beka Merbashvili</span>
+      </p>
+      <p className="aboutMe">
+        I'm a front-end developer passionate about building accessible web apps
+        that users love.
+      </p>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .imgBox {
+    width: 174px;
+    height: 240px;
+  }
+  .imgBox img {
+    width: 100%;
+    overflow: hidden;
+  }
+  .name {
+    font-size: 32px;
+    line-height: 40px;
+    text-align: center;
+    letter-spacing: -1.13px;
+    color: #ffffff;
+    width: 342px;
+  }
+  .name span {
+    text-decoration: underline #4ee1a0 4px;
+  }
+  .aboutMe {
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 26px;
+    text-align: center;
+    color: #d9d9d9;
+    width: 342px;
+  }
+`;
