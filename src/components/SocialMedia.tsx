e6e39ccc3ae7b40ad87d378e92995470a66c58ac
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-06-20 15:29:34 +0400

    add url at icnons

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,18 +5,20 @@ import linkedinIcon from "../assets/images/icon-linkedin.svg";
 import twitterIcon from "../assets/images/icon-twitter.svg";
 
 export default function SocialMedia() {
-  const socialMediaIcons = [
-    { name: "GitHub", icon: githubIcon },
-    { name: "Facebook", icon: facebookIcon },
-    { name: "LinkedIn", icon: linkedinIcon },
-    { name: "Twitter", icon: twitterIcon },
+  const socialMediaLinks = [
+    { name: "GitHub", icon: githubIcon, url: "https://github.com/Beka-Merebashvili" },
+    { name: "Facebook", icon: facebookIcon, url: "https://www.facebook.com/beqa.merebashvili.73" },
+    { name: "LinkedIn", icon: linkedinIcon, url: "https://www.linkedin.com/in/beka-merebashvili-a04554248/" },
+    { name: "Twitter", icon: twitterIcon, url: "https://twitter.com/merebashvili99" },
   ];
   return (
     <SocialNavigation>
       <h2>merebashvili</h2>
       <div className="navigation">
-        {socialMediaIcons.map((socialMedia, index) => (
-          <img key={index} src={socialMedia.icon} alt={socialMedia.name} />
+      {socialMediaLinks.map((socialMedia, index) => (
+          <a key={index} href={socialMedia.url} target="_blank" rel="noopener noreferrer">
+            <img src={socialMedia.icon} alt={socialMedia.name} />
+          </a>
         ))}
       </div>
     </SocialNavigation>
@@ -24,18 +26,18 @@ export default function SocialMedia() {
 }
 
 const SocialNavigation = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: 20px 0 0 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px 0 22px 0;
  h2 {
     font-weight: 700;
-font-size: 24px;
-line-height: 32px;
-letter-spacing: -0.33px;
-color: #FFFFFF;
-margin-bottom: 20px;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: -0.33px;
+    color: #FFFFFF;
+    margin-bottom: 20px;
  }
  .navigation {
     display: flex;
