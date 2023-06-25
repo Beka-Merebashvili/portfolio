c856b3fcb971e6236e4013f950c42a1c16e300dd
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-06-25 17:27:11 +0400

    add form component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import SocialMedia from "./SocialMedia";
 
 export default function Contact() {
   return (
@@ -10,6 +11,32 @@ export default function Contact() {
           fill in the form, and I'll get back to you as soon as possible.
         </p>
       </div>
+      <ContactForm>
+      <div className="name">
+        <input
+          type="text"
+          className="nameInpunt"
+          placeholder="NAME"
+        />
+      </div>
+      <div className="email">
+        <input
+          type="text"
+          className="nameInpunt"
+          placeholder="EMAIL"
+        />
+      </div>
+      <div className="name">
+        <input
+          type="text"
+          className="messageInpunt"
+          placeholder="MESSAGE"
+        />
+      </div>
+      <button>SEND MESSAGE</button>
+      </ContactForm>
+      <hr />
+      <SocialMedia />
     </ContactContainer>
   );
 }
@@ -43,4 +70,36 @@ const ContactContainer = styled.div`
     width: 342px;
     margin-top: 20px;
   }
+  hr {
+    width: 342px;
+    height: 1px;
+    background-color: #ffffff;
+    margin-bottom: 20px;
+  }
 `;
+
+
+
+const ContactForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  margin: 50px 0 86px 0;
+  input {
+    width: 342px;
+    height: 43px;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 26px;
+    letter-spacing: -0.22px ;
+    background-color: #242424;
+    border: none;
+    border-bottom: 1px solid #ffffff;
+    outline: none;
+    padding-left: 24px;
+  }
+  .messageInpunt {
+    height: 107px;
+  }
+`
